798b815d5e13312553c2882afcf8819d338dd2f7
Aleksey Smolin <ajikcey@mail.ru>
2021-07-06 13:26:17 +0300

    add vk

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-### Создайте новое приложение
-`npx create-react-app my-app`
+# Wikilib
 
-### Подключите VKUI
-`npm install @vkontakte/vkui || yarn add @vkontakte/vkui`
-
-### Установите пакет VK Bridge
-`npm install @vkontakte/vk-bridge || yarn add @vkontakte/vk-bridge`
+Все wiki-страницы ваших сообществ в одном приложении
 
 ### Использование HTTPS при разработке
+
 `set HTTPS=true&&npm start`
 
 https://vk.com/@vkappsdev-razrabotka-na-lokalnom-servere
 
 ### Запустите приложение
+
 `npm start`
--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import {View, ScreenSpinner, AdaptivityProvider, AppRoot} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './panels/Home';
+
+const App = () => {
+    const [activePanel, setActivePanel] = useState('home');
+    const [fetchedUser, setUser] = useState(null);
+    const [popout, setPopout] = useState(<ScreenSpinner size='large'/>);
+
+    useEffect(() => {
+        bridge.subscribe(({detail: {type, data}}) => {
+            if (type === 'VKWebAppUpdateConfig') {
+                const schemeAttribute = document.createAttribute('scheme');
+                schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+                document.body.attributes.setNamedItem(schemeAttribute);
+            }
+        });
+
+        async function fetchData() {
+            const user = await bridge.send('VKWebAppGetUserInfo');
+            setUser(user);
+            setPopout(null);
+        }
+
+        fetchData();
+    }, []);
+
+    const go = e => {
+        setActivePanel(e.currentTarget.dataset.to);
+    };
+
+    return (
+        <AdaptivityProvider>
+            <AppRoot>
+                <View activePanel={activePanel} popout={popout}>
+                    <Home id='home' fetchedUser={fetchedUser} go={go}/>
+                </View>
+            </AppRoot>
+        </AdaptivityProvider>
+    );
 }
 
 export default App;
